588695b120425fa89857c0d0b19740fea3ee2019
Joshua Lockhart <jlockhart@ncsasports.org>
2016-01-27 16:20:13 -0600

    school data seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+schools = School.create([{name: "University of Central Florida", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/15601college.jpg", mascot_name: "Knights", ipeds_id: 132903},
+{name:"University of Michigan", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/15654college.jpg", mascot_name: "Wolverines", ipeds_id: 170976},
+{name:"Vanderbilt University", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/15781college.jpg", mascot_name: "Commodores", ipeds_id: 221999},
+{name:"University of California - San Diego", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/15596college.jpg", mascot_name: "Tritons", ipeds_id: 110680},
+{name:"Pittsburg State University", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/15172college.jpg", mascot_name: "Gorillas", ipeds_id: 155681},
+{name:"Delta State University", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/14333college.jpg", mascot_name: "Fighting Okra", ipeds_id: 175616},
+{name:"Cardinal Stritch University", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/14086college.jpg", mascot_name: "Wolves", ipeds_id: 238430},
+{name:"Georgetown College", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/14599college.jpg", mascot_name: "Tigers", ipeds_id: 156745},
+{name:"Huntington University", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/14599college.jpg", mascot_name: "Foresters", ipeds_id: 150941},
+{name:"Kansas City Kansas Community College", logo: "https://recruit-match.ncsasports.org/fasttrack/clientimages/14673college.jpg", mascot_name: "Blue Devils", ipeds_id: 155292}])

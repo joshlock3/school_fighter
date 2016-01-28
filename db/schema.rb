71cf7e5baf759c401dc6dc49740df7e25444b57a
Jeremy Peterson <jpeterson@ncsasports.org>
2016-01-28 12:30:10 -0600

    Getting start message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127225622) do
+ActiveRecord::Schema.define(version: 20160128164614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,14 +57,20 @@ ActiveRecord::Schema.define(version: 20160127225622) do
     t.float    "longitude"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
+  create_table "user_preferences", force: :cascade do |t|
     t.integer  "season"
     t.integer  "school_size"
     t.integer  "location_type"
     t.string   "zip"
-    t.integer  "sport_acedemic_balance"
     t.integer  "sport_id"
+    t.integer  "sport_acedemic_balance"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",                null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float    "latitude"
